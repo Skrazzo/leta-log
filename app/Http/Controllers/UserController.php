654672649825256c4ce31fc27396b8d7b4714750
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-05-22 16:39:31 +0300

    Before adding typescript

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function logout(Request $req){
-        auth()->logout();
+    public function logout()
+    {
+        Auth::logout();
         return redirect(route('login'));
     }
 
-    public function register(Request $req){
+    public function register(Request $req)
+    {
         $data = $req->validate([
-            'username' => 'required|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required|min:3',
+            'surname' => 'required',
             'password' => 'required|min:8'
         ]);
 
@@ -22,17 +27,18 @@ class UserController extends Controller
         return back();
     }
 
-    public function login(Request $req){
+    public function login(Request $req)
+    {
         $credentials = $req->validate([
             'username' => 'required',
             'password' => 'required',
         ]);
- 
+
         if (auth()->attempt($credentials)) {
             $req->session()->regenerate();
             return redirect(route('dashboard'));
         }
- 
+
         return back()->withErrors([
             'error' => 'Username or password is incorrect!',
         ]);
---
+++ resources/js/components/FormInput.jsx
@@ -0,0 +1,7 @@
+export default FormInput = ({ text }) => {
+    return (
+        <label for="">
+            <input type="text" name="input" id="input" />
+        </label>
+    );
+};
